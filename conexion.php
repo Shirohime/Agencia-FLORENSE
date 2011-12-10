7bd6ecf18d2bba4f1d4069b3ddbb6ba9ffc96e80
José Manuel <metalFranckie@gmail.com>
2011-12-09 18:53:21 -0600

    mensaje de commit

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysql_connect('localhost','root','') or die ("No se ha podido conectar");
+$con = mysql_connect('localhost','aeadmin','p4ss') or die ("No se ha podido conectar");
 
 #Seleccion de la base de datos a utilizar
 mysql_select_db("agenciaenfermeria",$con) or die("Error al tratar de selecccionar la base de datos");
